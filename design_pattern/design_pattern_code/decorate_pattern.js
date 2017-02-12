746e0c568a17970d6fcdcac83e0dbf5d4e3bc205
bijh <bijh@uyunsoft.cn>
2017-02-12 20:04:22 +0800

    reset file folder

---
+++ design_pattern/design_pattern_code/decorate_pattern.js
@@ -0,0 +1,55 @@
+var tree = {
+}
+
+tree.decorate = function () {
+  console.log('show tree before decorate');
+}
+
+tree.getDecorate = function (decorate) {
+  tree[decorate].prototype = this;
+  return new tree[decorate];
+}
+
+tree.summer = function () {
+  this.decorate = function () {
+    this.summer.prototype.decorate();
+    this.summer.color = 'green';
+    console.log('tree in summer, color: ' + this.summer.color);
+  }
+}
+
+tree.autumn = function () {
+  this.decorate = function () {
+    this.autumn.prototype.decorate();
+    this.autumn.color = 'green';
+    console.log('tree in autumn, color: ' + this.autumn.color);
+  }
+}
+
+tree.winter = function () {
+  this.decorate = function () {
+    this.winter.prototype.decorate();
+    this.winter.color = 'green';
+    console.log('tree in winter, color: ' + this.winter.color);
+  }
+}
+
+tree.spring = function () {
+  this.decorate = function () {
+    this.spring.prototype.decorate();
+    this.spring.color = 'green';
+    console.log('tree in spring, color: ' + this.spring.color);
+  }
+}
+
+var a = new Object(tree);
+a = a.getDecorate('spring');
+a = a.getDecorate('summer');
+a = a.getDecorate('autumn');
+a = a.getDecorate('winter');
+a.decorate();
+
+
+/*
+**
+*/
