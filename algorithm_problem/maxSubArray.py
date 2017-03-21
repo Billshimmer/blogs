9dc507a7d2cfe307ea00e2ce04f36102c1e12b0a
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-03-21 10:19:54 +0800

    maxSubArray

--- algorithm_problem/maxSubArray.py
+++ algorithm_problem/maxSubArray.py
@@ -1,19 +1,27 @@
 
-
 def maxSubArray(alist):
+  curSum = 0 
+  maxSum = 0 
+  start_index, end_index, maxSum_start, maxSum_end = 0, 0, 0, 0
 
-    curSum = 0
-    maxSum = 0
+  for i in range(0, len(alist), 1):
+    curSum += alist[i]
 
-    for i in range(0, len(alist), 1):
-        curSum += alist[i]
-        if curSum < 0:
-            curSum = 0
-        if curSum > maxSum:
-            maxSum = curSum
-    
-    return maxSum
+    if curSum < 0:
+      curSum = 0
+      start_index = i + 1
+      end_index = i + 1
+    if curSum > maxSum:
+      maxSum = curSum
+      end_index = i
+      maxSum_start = start_index
+      maxSum_end = end_index
+  
+  print maxSum_start, maxSum_end
 
-alist = [1, -2, 3, 10, -4, 7, 2, -5]
+  return maxSum
+
+
+alist = [1, -2, 3, 10, -4, 7, 2, -100]
 
 print maxSubArray(alist)
