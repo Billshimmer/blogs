17b54d964e7137cc9be61e79d5a779176ac16696
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-03-23 15:55:04 +0800

    PD

--- robots/robot.py
+++ robots/robot.py
@@ -3,45 +3,65 @@ import requests
 import re
 import os
 
+URL = "https://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word="
 
-def downloadPic(html, keyword):
+class PD:
 
-    def mkdir(keyword):
+    def __init__(self):
+        self.enable = False
+        self.page_index = 0
+        self.pic_index = 1
+        self.keyword = ""
+    # 页面代码
+    def getPageCode(self):
+        url =  URL + str(self.keyword) + "&pn=" + str(self.page_index*20)
+        self.page_index += 1
+        result = requests.get(url)
+        
+        return result
+    # 新建目录
+    def mkdir(self):
+        keyword = self.keyword
         imagePath = os.getcwd() + "/images"
         dirPath = imagePath + "/" + keyword
-
+        
         print "找到关键词为" + keyword + "的图片, 现在开始下载图片...."
         print "图片存储在当前images目录下对应子目录下:"
         if not os.path.exists(imagePath):
             os.mkdir(os.getcwd() + "/images/")
         if not os.path.exists(dirPath):
             os.mkdir(os.getcwd() + "/images/" + keyword)
+    # 下载图片
+    def downloadPic(self, html):
+        keyword = self.keyword
+        pic_url = re.findall('"objURL":"(.*?)",', html, re.S)
+        self.mkdir()
 
-    pic_url = re.findall('"objURL":"(.*?)",', html, re.S)
-    pic_index = 0
-    mkdir(keyword)
+        for item in pic_url:
+            print "正在下载第" + str(self.pic_index) + "张图片, 图片下载地址是" + str(item)
+            try:
+                pic = requests.get(item, timeout=10)
+            except requests.exceptions.ConnectionError:
+                print "some error happened"
+                continue
 
-    for item in pic_url:
-        print "正在下载第" + str(pic_index + 1) + "张图片, 图片下载地址是" + str(item)
-        try:
-            pic = requests.get(item, timeout=10)
-        except requests.exceptions.ConnectionError:
-            print "some error happened"
-            continue
+            string = './images/' + str(keyword) + "/" + str(keyword) + '_' + str(self.pic_index) + '.jpg'
+            fp = open(string, 'wb')
+            fp.write(pic.content)
+            fp.close()
 
-        string = './images/' + keyword + "/" + str(keyword) + '_' + str(pic_index) + '.jpg'
-        fp = open(string, 'wb')
-        fp.write(pic.content)
-        fp.close()
+            self.pic_index += 1
+    # 开始或者结束 
+    def start(self):
+        self.enable = True
+        while not self.keyword:
+            self.keyword = raw_input("input key word for images which you want:")
+        while self.enable == True:
+            pageCode = self.getPageCode()
+            self.downloadPic(pageCode.text)
+            user_input = raw_input("Enter for next page OR Q for exit:")
+            if user_input == 'Q':
+                self.enable = False
+p = PD()
+p.start()
 
-        pic_index += 1
-
-
-if __name__ == '__main__':
-    word = ""
-    while not word:
-        word = raw_input("input key word for images which you want:")
-    url = "https://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=" + \
-        word + "&ct=201326592&v=flip"
-    result = requests.get(url)
-    downloadPic(result.text, word)
