a08f0b9305ec6eced62c344d59bc66c89c62b997
bijh <bijh@uyunsoft.cn>
2017-03-26 12:58:37 +0800

    length_of_lis

--- algorithm_problem/length_of_lis.py
+++ algorithm_problem/length_of_lis.py
@@ -17,6 +17,24 @@ def lengthOfLIS(nums):
         if i == size:
             size += 1
     return size
-    
+
+def AlengthOfLIS(nums):
+    tails = [1 for i in range(len(nums))]
+    size = 0
+    for i in range(0, len(nums), 1):
+        for j in range(0, i+1, 1):
+            if nums[i] > nums[j]:
+                tails[i] = max(tails[i], tails[j]+1)
+
+        size = max(size, tails[i])
+
+    return  size
+
+
+
 array = [10, 9, 2, 4,5, 3, 7, 101, 18,19]
+
 print lengthOfLIS(array)
+print AlengthOfLIS(array)
+
+
