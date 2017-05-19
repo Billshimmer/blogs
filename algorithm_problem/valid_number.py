232e937397d17fbfdce807d68b311df6f5eeeef0
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-05-19 14:09:11 +0800

    valid_number

--- algorithm_problem/valid_number.py
+++ algorithm_problem/valid_number.py
@@ -3,9 +3,41 @@
 
 
 class Solution(object):
-    def idNumber(self, s):
-        return True
-    
+    def isNumber(self, s):
+        
+        is_number = False
+        is_exp = True
+        s = s.strip()
+        length = len(s)
+        index = 0
 
-    
-Solution().isNumber('-3.2e-23')
+        if index < length and (s[index] == '+' or s[index] == '-'):
+            index += 1
+        
+        while index < length and s[index].isdigit():
+            index += 1
+            is_number = True
+        
+        if index < length and s[index] == '.':
+            index += 1
+            while index < length and s[index].isdigit():
+                index += 1
+                is_number = True
+        
+        if index < length and (s[index] == 'e' or s[index] == 'E'):
+            is_exp = False
+            index += 1
+            if index < length and (s[index] == '+' or s[index] == '-'):
+                index += 1
+            while index < length and s[index].isdigit():
+                index += 1
+                is_exp = True
+
+        return is_number and is_exp and index == length
+
+
+print Solution().isNumber('-3.2e-23')
+print Solution().isNumber('1223123')
+print Solution().isNumber('qwdkjhqwjd')
+print Solution().isNumber('3.223535')
+print Solution().isNumber('3.2e32')
