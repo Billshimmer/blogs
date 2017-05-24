400846f06eefaa79fc40e37ec5a7b7cb665b9707
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-05-24 14:47:55 +0800

    fix

--- algorithm_problem/sqrt.py
+++ algorithm_problem/sqrt.py
@@ -3,12 +3,14 @@
 
 # leetcode 69
 
+
 class Solution(object):
     def sqrt(self, x):
         exp = 0.1
         result = 1.0
         while abs(result * result - x) > exp:
-            result = result - result / 2 - x / result / 2
+            result = result / 2 + x / result / 2
         return int(result)
 
-print Solution().sqrt(5)
+
+print Solution().sqrt(10)
