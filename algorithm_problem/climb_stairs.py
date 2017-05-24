e858b0391be9eafb36876827fe8cbdc93414450b
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-05-24 16:04:58 +0800

    climb_stairs

---
+++ algorithm_problem/climb_stairs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+# leetcode 70
+
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        dp = [ 0 for __ in range(n) ]
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2, n, 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n-1]
+
+
+print Solution().climbStairs(19)
