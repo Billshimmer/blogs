76e41a6c753c000250a8d5b3557a4c9bdfed8663
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-05-24 14:42:00 +0800

    sqrt

---
+++ algorithm_problem/sqrt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+# leetcode 69
+
+class Solution(object):
+    def sqrt(self, x):
+        exp = 0.1
+        result = 1.0
+        while abs(result * result - x) > exp:
+            result = result - result / 2 - x / result / 2
+        return int(result)
+
+print Solution().sqrt(5)
