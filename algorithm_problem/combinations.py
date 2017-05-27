11e09c446a28f4b8d738ebdc5b6f94d6ae8c933e
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-05-27 12:03:16 +0800

    combination

---
+++ algorithm_problem/combinations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+from collections import defaultdict
+
+# leetcode 77
+
+
+class Solution(object):
+    def combinations(self, n, k):
+        if k == 1:
+            return [[i + 1] for i in range(n)]
+        result = []
+        if n > k:
+            result = [r + [n] for r in self.combinations(n - 1, k - 1)] + self.combinations(n - 1, k)
+        else:
+            result = [r + [n] for r in self.combinations(n - 1, k - 1)]
+        return result
+
+print Solution().combinations(4, 2)
